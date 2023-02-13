4db89fc56c35ece51fe2a58efce21b8d62545a81
nomentech <nomentech@gmail.com>
2023-02-13 14:03:11 +0800

    improve header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
-import { useBoardDispatch } from '../context/BoardContext'
+import { useBoard, useBoardDispatch } from '../context/BoardContext'
 import { BOARD_LIST } from '../minesweeper/contants'
 import { Board } from '../minesweeper/types'
 
 export default function Header() {
+  const current = useBoard()
   const boardDispatch = useBoardDispatch()
 
-  function handleClick(board: Board) {
-    boardDispatch({ type: 'reset_board', payload: { board } })
+  function handleClick(selected: Board) {
+    if (!isSame(current, selected)) {
+      boardDispatch({ type: 'reset_board', payload: { board: selected } })
+    }
+  }
+
+  function isSame(current: Board, selected: Board) {
+    return current.level === selected.level
   }
 
   return (
     <div className="header">
       {BOARD_LIST.map((board) => (
-        <div key={board.level} onClick={() => handleClick(board)}>
+        <div
+          key={board.level}
+          onClick={() => handleClick(board)}
+          className={`${isSame(current, board) && 'selected'}`}
+        >
           {board.level}
         </div>
       ))}
